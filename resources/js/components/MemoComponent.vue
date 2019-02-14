e0e831345017a84c6bbc5f06b3625113a8539dc5
mohdhj <pc-moon@hotmail.com>
2019-02-14 22:32:40 +0300

    Develop it

--- resources/js/components/MemoComponent.vue
+++ resources/js/components/MemoComponent.vue
@@ -1,11 +1,40 @@
 <template>
     <div>
-      <input type="text" class="form-control" v-model="newMemoTxt" v-on:keyup.enter="newMemo" />
-      <ul>
-        <li v-for="memo in memos">
-          {{memo}}
-        </li>
-      </ul>
+
+      <el-tabs v-model="activeName" type="card" style="width: 100%">
+        <el-tab-pane label="Memos" name="memos">
+        <el-table
+          :data="memos.filter(data => !search || data.title.toLowerCase().includes(search.toLowerCase()))"
+          style="width: 100%">
+            <el-table-column type="expand">
+              <template slot-scope="props">
+                <textarea class="form-control" v-model="props.row.title">
+                </textarea>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="title"
+              label="Title"
+              sortable
+              filterable
+              width="180">
+                <template slot="header" slot-scope="scope">
+                  <el-input
+                    v-model="search"
+                    size="mini"
+                    placeholder="Type to search"/>
+                    Title
+                </template>
+              </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="newMemo" name="newMemo">
+          <textarea class="form-control" v-model="newMemoTxt"></textarea>
+          <button @click="newMemo" class="btn btn-primary btn-block btn-lg">Save</button>
+        </el-tab-pane>
+      </el-tabs>
+
+
     </div>
 </template>
 
@@ -13,14 +42,24 @@
     export default {
         data() {
             return {
+              activeName: 'memos',
+              search: '',
               newMemoTxt: '',
-              memos: []
+              memos: [
+                {'title':'First Memo'}
+              ]
             };
         },
         methods:{
           newMemo(){
-            this.memos.push(this.newMemoTxt);
+            this.memos.push({'title':this.newMemoTxt});
             this.newMemoTxt = '';
+            this.$notify({
+              title: 'Success',
+              message: 'Memo Saved',
+              type: 'success'
+            });
+
           }
         }
     }
